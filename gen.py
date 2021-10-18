1789dc228022ee299b8dd8f01a255fe41fda2471
maciejszulia <maciej.szulia@gmail.com>
2021-10-18 02:25:05 +0200

    fixed a typo

--- gen.py
+++ gen.py
@@ -1,8 +1,10 @@
 import string
 import random
 
+global PASSWORD
 
-def get_password(pasword_len=8, has_lower=True, has_upper=True, has_special=False):
+
+def get_password(password_len=8, has_lower=True, has_upper=True, has_special=False):
     available_cases = string.digits
 
     if has_lower is True:
@@ -13,7 +15,7 @@ def get_password(pasword_len=8, has_lower=True, has_upper=True, has_special=Fals
         available_cases += string.punctuation
 
     output = ""
-    for i in range(pasword_len):
+    for i in range(password_len):
         output += available_cases[random.randint(0, len(available_cases) - 1)]
 
     return output
