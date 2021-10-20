7d3094270265ffa78ee6de8199e9d69992010898
maciejszulia <maciej.szulia@gmail.com>
2021-10-21 00:43:18 +0200

    checkboxes now working

--- gen.py
+++ gen.py
@@ -4,12 +4,15 @@ import random
 global PASSWORD
 
 
+# #1 todo: add digits switch
+# #2 todo: add exception when no available cases
+
 def get_password(password_len=8, has_lower=True, has_upper=True, has_special=False):
     available_cases = string.digits
-
+    print(available_cases)
     if has_lower is True:
         available_cases += string.ascii_lowercase
-    if has_upper:
+    if has_upper is True:
         available_cases += string.ascii_uppercase
     if has_special:
         available_cases += string.punctuation
@@ -19,3 +22,6 @@ def get_password(password_len=8, has_lower=True, has_upper=True, has_special=Fal
         output += available_cases[random.randint(0, len(available_cases) - 1)]
 
     return output
+
+
+print(get_password(24))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import random
 import pyperclip
 import string
@@ -6,30 +7,34 @@ import gen
 
 root = tk.Tk()
 
-global password
-global tickbox
-tickbox = tk.BooleanVar()
+password_len = tk.IntVar()
+has_lower_value = tk.BooleanVar()
+has_upper_value = tk.BooleanVar()
+has_special_value = tk.BooleanVar()
 
 
-def call_password(password_len=8, has_lower=True, has_upper=True, has_special=False):
-    password = gen.get_password(has_lower=tickbox)
+def call_password(password_len=8, has_lower=True, has_upper=False, has_special=False):
+    password = gen.get_password(8, has_lower=has_lower_value.get(),
+                                has_upper=has_upper_value.get(), has_special=has_special_value.get())
 
     password_label = tk.Label(text=password, padx=5, pady=5, cursor='xterm')
-    password_label.grid(row=1, column=0)
+    password_label.grid(row=2, column=0)
 
     copy_button = tk.Button(text="copy", command=pyperclip.copy(password))
-    copy_button.grid(row=1, column=1)
+    copy_button.grid(row=2, column=1)
+    print(has_upper_value.get())
 
 
-
-
-has_lower_box = tk.Checkbutton(text="lower chars", variable=tickbox)
+has_lower_box = tk.Checkbutton(root, text="lower chars", variable=has_lower_value)
 has_lower_box.grid(row=0, column=1)
 
-gen_button = tk.Button(text="generate", padx=40, pady=20, command=lambda: call_password(has_lower=tickbox))
-gen_button.grid(row=0, column=0, columnspan=1)
+has_upper_box = tk.Checkbutton(root, text="upper chars", variable=has_upper_value)
+has_upper_box.grid(row=1, column=1)
+
+gen_button = tk.Button(text="generate", padx=40, pady=20, command=lambda: call_password())
+gen_button.grid(row=0, column=0, columnspan=1, rowspan=2)
 
 root.mainloop()
-
+print(has_upper_value.get())
 # while True:
 #     print(tickbox)
